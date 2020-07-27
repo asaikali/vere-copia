c9e81ac3a0de551ca05feb47e47ac27aa9f8857b
Adib Saikali <asaikali@gmail.com>
2020-07-27 16:49:50 -0400

    Add project skeleton

---
+++ README.md
@@ -0,0 +1,4 @@
+# vere-copia
+
+A sample set of microservices for a store to demo microservices observability. 
+
---
+++ store-kiosk/src/test/java/com/example/kiosk/KioskApplicationTest.java
@@ -0,0 +1,11 @@
+package com.example.kiosk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KioskApplicationTest {
+
+  @Test
+  void contextLoads() {}
+}
