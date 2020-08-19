172bf47b986df3c8322074b8a79b4d66e62bb068
Howard Yoo <yooh@vmware.com>
2020-08-19 14:20:23 -0500

    added loadgen.sh to generate random load on store operations added version number 2.0.0 on spring boot starter (required)

---
+++ loadgen.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Send requests every $1 seconds"
+skus=( 101 102 103 104 200 201 202 300 301 302 303 400 401 500 501 502 )
+while [ true ]
+do
+  s_id=$(( RANDOM % ${#skus[@]} ))
+  sku=${skus[s_id]}
+	quantity=$((1 + RANDOM % 10))
+	curl_cmd="curl -X GET 'http://localhost:8080/stores/stock?sku=${sku}&quantity=${quantity}'"
+	echo "$curl_cmd"
+	eval "$curl_cmd"
+	sleep "$1"
+	printf "\n"
+done
