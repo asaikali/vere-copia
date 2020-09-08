6c5da727aecaa45546b4c3a766f84add2dd5a764
Adib Saikali <asaikali@gmail.com>
2020-09-08 00:31:35 -0400

    add angular distributed tracing

--- store-kiosk/src/app/app.module.ts
+++ store-kiosk/src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MaterialModule} from "./material/material.module";
 import {ReactiveFormsModule} from "@angular/forms";
-import {HttpClientModule} from "@angular/common/http";
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import { SkuInventoryDetailComponent } from './sku-inventory-detail/sku-inventory-detail.component';
 import { SearchComponent } from './search/search.component';
+import {ZipkinHttpInterceptor} from "./zipkin.http.interceptor";
 @NgModule({
   declarations: [
     AppComponent,
@@ -23,7 +24,9 @@ import { SearchComponent } from './search/search.component';
     ReactiveFormsModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: ZipkinHttpInterceptor, multi: true}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ store-kiosk/src/app/zipkin.http.interceptor.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@angular/core';
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import * as zipkin from 'zipkin';
+import * as zipkinTransportHttp from 'zipkin-transport-http';
+
+@Injectable()
+export class ZipkinHttpInterceptor implements HttpInterceptor {
+
+    private readonly tracer: zipkin.Tracer;
+    private readonly instrumentation: zipkin.Instrumentation.HttpClient;
+
+    constructor() {
+        const localServiceName = 'store-kiosk';
+        const remoteServiceName = 'store-operations';
+
+        this.tracer = new zipkin.Tracer({
+            ctxImpl: new zipkin.ExplicitContext(),
+            recorder: new zipkin.BatchRecorder({
+                logger: new zipkinTransportHttp.HttpLogger({
+                    endpoint: 'http://localhost:9411/api/v2/spans',
+                    jsonEncoder: zipkin.jsonEncoder.JSON_V2
+                })
+            }),
+            localServiceName: localServiceName,
+            traceId128Bit: true
+        });
+        this.instrumentation = new zipkin.Instrumentation.HttpClient(
+            { tracer: this.tracer, serviceName: localServiceName, remoteServiceName: remoteServiceName }
+        );
+
+    }
+
+    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        return new Observable(observer => {
+            this.tracer.scoped(() => {
+                const options = this.instrumentation.recordRequest({ url: request.url, headers: {} }, request.url, request.method || 'GET');
+                request = request.clone({
+                    setHeaders: options.headers as any
+                });
+
+                const traceId = this.tracer.id
+                next.handle(request).pipe(tap((event: HttpEvent<any>) => {
+                    if (event instanceof HttpResponse) {
+                        this.tracer.scoped(() => {
+                            if (event.ok) {
+                                this.instrumentation.recordResponse(traceId, event.status.toString())
+                            } else {
+                                this.instrumentation.recordError(traceId, new Error('status ' + event.status))
+                            }
+                        })
+                    }
+                })).subscribe(event => observer.next(event));
+            });
+        });
+    }
+}
